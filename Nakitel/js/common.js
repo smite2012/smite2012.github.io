59727628843e9be912824f3c6a9aa0ebcf390e58
smite2012 <33417399+smite2012@users.noreply.github.com>
2017-12-05 12:14:03 +0200

    Nakitel

--- Nakitel/js/common.js
+++ Nakitel/js/common.js
@@ -1,4 +1,12 @@
 
+$(".toggle_mnu").click(function() {
+		$(".sandwich").toggleClass("activee");
+		$(".menu").slideToggle("slow");
+		$(".menu_text").toggle("slide");
+	});
+
+$(".menu ul a").mPageScroll2id();
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -37,29 +45,74 @@ function showSlides(n) {
 	dots[slideIndex-1].className+= " active";
 }
 
-$(".toggle_mnu").click(function() {
-		$(".sandwich").toggleClass("activee");
-	});
+var tab;
+var tabContent;
+var tabHeader;
 
-$(".top_mnu ul a").click(function() {
-		$(".top_mnu").fadeOut(600);
-		$(".sandwich").toggleClass("activee");
-		$(".top_text").css("opacity", "1");
-	}).append("<span>");
-
-	$(".toggle_mnu").click(function() {
-		if ($(".top_mnu").is(":visible")) {
-			$(".top_text").css("opacity", "1");
-			$(".top_mnu").fadeOut(600);
-			$(".top_mnu li a").removeClass("fadeInUp animated");
-		} else {
-			$(".top_text").css("opacity", ".1");
-			$(".top_mnu").fadeIn(600);
-			$(".top_mnu li a").addClass("fadeInUp animated");
-		};
-	});
+window.onload=function () {
+	tabContent=document.getElementsByClassName('tabContent');
+	tab=document.getElementsByClassName('tab');
+	tabHeader=document.getElementById('tab_header_main');
+	hideTabsContent(1);
+	showTabsContent(1);
 
 
+	
+}
+
+function hideTabsContent(a) {
+	for (var i=a; i<tabContent.length; i++){
+		tabContent[i].classList.remove('show');
+		tabContent[i].classList.add('hide');
+		tab[i].classList.remove('whiteborder');
+	}
+}
+
+document.getElementById('tabs').onclick=function (event) {
+	var target=event.target;
+	if (target.className=='tab'){
+		for (var i=0; i<tab.length; i++){
+			if(target == tab[i]){
+				showTabsContent(i);
+				break;
+			}
+		}
+	}
+}
+
+function showTabsContent(b) {
+	if (tabContent[b].classList.contains('hide')){
+		hideTabsContent(0);
+		tab[b].classList.add('whiteborder');
+		tabContent[b].classList.remove('hide');
+		tabContent[b].classList.add('show');
+		
+		
+		tabHeader.textContent = tab[b].innerHTML;
+		// tabHeader.css("animation:fade");
 
 
+		
 
+
+	}
+}
+
+
+function openDoor(field) {
+    var y = $(field).find(".thumb");
+    var x = y.attr("class");
+    var a = $(field).find(".thumb2");
+    var b = a.attr("class");
+
+    if (y.hasClass("thumbOpened") || a.hasClass("thumbOpened2")) {
+        y.removeClass("thumbOpened");
+        a.removeClass("thumbOpened2");
+    }
+    else {
+        $(".thumb").removeClass("thumbOpened");
+        $(".thumb2").removeClass("thumbOpened2");
+        y.addClass("thumbOpened");
+        a.addClass("thumbOpened2");
+    }
+}
