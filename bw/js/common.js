c4d262cb8e5210ccb7a6be6e2f24291ff0a2da1a
smite2012 <33417399+smite2012@users.noreply.github.com>
2017-12-23 10:16:39 +0200

    bw

---
+++ bw/js/common.js
@@ -0,0 +1,32 @@
+ var swiper = new Swiper('.swiper-container', {
+      direction: 'vertical',
+       slidesPerView: 'auto',
+       spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+
+ $("input, textarea").jqBootstrapValidation();
+
+	$(".menu_wrap ul li a").mPageScroll2id();
+
+ // preloader
+$(window).on('load', function () {
+	$('.preloader').delay(1000).fadeOut('slow');
+})
+
+// back to top button
+var $btnTop = $(".btn-top")
+$(window).on("scroll", function(){
+	if ($(window).scrollTop() >= 20){
+		$btnTop.fadeIn();
+	}else{
+		$btnTop.fadeOut();
+	}
+});
+// back to top button click
+$btnTop.on("click", function(){
+	$("html,body").animate({scrollTop:0},900)
+});
